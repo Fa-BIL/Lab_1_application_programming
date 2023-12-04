64eacac0671e40a294c618f7ec1646d0569f7944
Savenkov Alexander <Fa.BIL@yandex.ru>
2023-12-04 12:52:21 +0400

    v2

--- parse.py
+++ parse.py
@@ -58,3 +58,59 @@ def main(rewiews_limit: int, dir_path: str) -> None:
     driver.close()
     driver.quit()
 
+# def main(rewiews_limit: int, dir_path: str) -> None:
+#     driver = webdriver.Edge()
+#     driver.maximize_window()
+#     good_comment_num = 1
+#     bad_comment_num = 1
+
+#     while True:
+#         film = (f"https://www.kinopoisk.ru/film/{random.randint(1000, 100000)}/reviews/ord/rating/status/all/perpage/200/page/")
+#         if good_comment_num>= rewiews_limit and bad_comment_num >= rewiews_limit:
+#             break
+#         for page in range(1, 5):
+#             try:
+#                 driver.get(film + str(page) + "/")
+#             except:
+#                 continue
+#             sleep(10)
+#             soup = BeautifulSoup(driver.page_source, "lxml")
+#             if soup == 404:
+#                 break
+#             try:
+#                 driver.find_element(By.CLASS_NAME, "CheckboxCaptcha-Button")
+#             except:
+#                 pass
+#             else:
+#                 driver.find_element(By.CLASS_NAME, "CheckboxCaptcha-Button").click()
+#                 sleep(10)
+#             film_name = (
+#                 BeautifulSoup(str(soup.find("a", class_="breadcrumbs__link")), "lxml").text
+#                 + "\n"
+#             )
+            
+#             for comment in soup.find_all("div", class_="response good"):
+#                 if good_comment_num <= rewiews_limit:
+#                     comment = BeautifulSoup(str(comment), "lxml").findChild(
+#                         "span", class_="_reachbanner_"
+#                     )
+#                     with open(
+#                         os.path.join(dir_path, "dataset", "good", str(good_comment_num).zfill(4)), "w", encoding="utf-8"
+#                     ) as file:
+#                         file.write(film_name)
+#                         file.write(BeautifulSoup(str(comment), "lxml").text)
+#                         good_comment_num += 1
+#             for comment in soup.find_all("div", class_="response bad"):
+#                 if bad_comment_num <= rewiews_limit:
+#                     comment = BeautifulSoup(str(comment), "lxml").findChild(
+#                         "span", class_="_reachbanner_"
+#                     )
+#                     with open(os.path.join(dir_path, "dataset", "bad", str(bad_comment_num).zfill(4)), "w", encoding="utf-8") as file:
+#                         file.write(film_name)
+#                         file.write(BeautifulSoup(str(comment), "lxml").text)
+#                         bad_comment_num += 1
+#             if soup.find("li", class_ = "arr") == None:
+#                 break
+#             sleep(3)
+#     driver.close()
+#     driver.quit()
