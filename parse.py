cd888f592073d65156f944c5432ae444d58c8a65
Savenkov Alexander <Fa.BIL@yandex.ru>
2023-12-04 12:38:03 +0400

    def main and driver

--- parse.py
+++ parse.py
@@ -13,4 +13,23 @@ def main(rewiews_limit: int, dir_path: str) -> None:
     driver.maximize_window()
     good_comment_num = 1
     bad_comment_num = 1
+    for film in films:
+        for page in range(1, 5):
+            try:
+                driver.get(f"https://www.kinopoisk.ru/film/{film}/reviews/ord/rating/status/all/perpage/200/page/" + str(page) + "/")
+            except:
+                continue
+            sleep(10)
+            soup = BeautifulSoup(driver.page_source, "lxml")
+            try:
+                driver.find_element(By.CLASS_NAME, "CheckboxCaptcha-Button")
+            except:
+                pass
+            else:
+                driver.find_element(By.CLASS_NAME, "CheckboxCaptcha-Button").click()
+                sleep(10)
+            film_name = (
+                BeautifulSoup(str(soup.find("a", class_="breadcrumbs__link")), "lxml").text
+                + "\n"
+            )
 
