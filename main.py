c64effd729f7cfe018b257b1560a01b14a3ed210
Savenkov Alexander <Fa.BIL@yandex.ru>
2023-12-04 12:32:43 +0400

    imports and main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from parse import main
+
+if __name__ == "__main__":
+   limit = int(input())
+   main(limit, "")
+    
--- parse.py
+++ parse.py
@@ -1,1 +1,12 @@
+from time import sleep
 import os
+import random
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+
+
+
