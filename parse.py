0d2d74860d06e9bdbb7f77d527f985e2718060e6
Savenkov Alexander <Fa.BIL@yandex.ru>
2023-12-04 12:34:26 +0400

    films

--- parse.py
+++ parse.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
+films = ["522094", "471410", "478052", "1294123", "677638", "436225", "259991", "396473", "276129", "81620", "535341", "326"]
 
 
 
