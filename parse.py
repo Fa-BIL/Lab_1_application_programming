b4e3b68b714c3fb4ceadaa87f07b7137d7a7a99c
Savenkov Alexander <Fa.BIL@yandex.ru>
2023-12-04 12:36:01 +0400

    def main

--- parse.py
+++ parse.py
@@ -8,5 +8,9 @@ from selenium.webdriver.common.by import By
 
 films = ["522094", "471410", "478052", "1294123", "677638", "436225", "259991", "396473", "276129", "81620", "535341", "326"]
 
-
+def main(rewiews_limit: int, dir_path: str) -> None:
+    driver = webdriver.Edge()
+    driver.maximize_window()
+    good_comment_num = 1
+    bad_comment_num = 1
 
